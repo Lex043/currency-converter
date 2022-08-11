1b839698523d8ee4e42cc8d1df134aaf5dff52bd
Oyebamiji Alex <alexferanmi390@gmail.com>
2022-08-11 16:22:15 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,52 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import InputAmtandCurrency from "./components/InputAmtandCurrency";
 
 function App() {
-  const host = "api.frankfurter.app";
+  const [currency, setCurrency] = useState([]);
+  const [text, setText] = useState("");
+
+  //
+  // const urls = [
+  //   `https://${host}/latest?amount=${text}&from=GBP&to=USD`,
+  //   `https://${host}/currencies`,
+  // ];
+
+  const fetchData = async () => {
+    try {
+      const response = await Promise.all(
+        urls.map((url) => fetch(url).then((res) => res.json()))
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // fetchData();
+
   useEffect(() => {
-    fetch(`https://${host}/latest`)
-      .then((response) => response.json())
+    const host = "api.frankfurter.app";
+    fetch(`https://${host}/currencies`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
       .then((data) => {
         console.log(data);
+        setCurrency(data);
       })
-      .catch((error) => console.log(error));
-  }, []);
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, [setCurrency, text]);
 
   return (
-    <div>
-      <InputAmtandCurrency />
+    <div className="flex flex-col h-screen justify-center items-center">
+      <InputAmtandCurrency text={text} setText={setText} currency={currency} />
     </div>
   );
 }
--- src/components/InputAmtandCurrency.jsx
+++ src/components/InputAmtandCurrency.jsx
@@ -1,40 +1,76 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-const InputAmtandCurrency = () => {
-  const [currency, setCurrency] = useState([]);
+const InputAmtandCurrency = ({ currency, text, setText }) => {
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
 
-  const host = "api.frankfurter.app";
-  useEffect(() => {
-    fetch(`https://${host}/currencies`)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setCurrency(data);
-      })
-      .catch((error) => console.log(error));
-  }, [setCurrency]);
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
 
-  console.log(currency);
+  const handleFromChange = (e) => {
+    setFrom(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleToChange = (e) => {
+    setTo(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const host = "api.frankfurter.app";
+      const response = await fetch(
+        `https://${host}/latest?amount=${text}&from=${from}&to=${to}`
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <section>
-      <form action="">
-        <div>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 sm:flex-row">
+        <div className="flex flex-col">
+          <label>Amount</label>
+          <input
+            type="text"
+            value={text}
+            onChange={handleChange}
+            className="border-current border-2 h-12 w-full rounded-md outline-none"
+          />
+        </div>
+
+        <div className="flex flex-col">
           <label>From</label>
-          <select name="" id="">
+          <select
+            value={from}
+            onChange={handleFromChange}
+            className="border-current border-2 h-12 w-full rounded-md outline-none"
+          >
+            <option value="">None</option>
             {Object.keys(currency).map((key, index) => (
               <option key={index} value={key}>
-                {key}
+                {key}- {currency[key]}
               </option>
             ))}
           </select>
         </div>
-        <div>
+        <div className="flex flex-col">
           <label>To</label>
-          <select name="" id="">
+          <select
+            value={to}
+            onChange={handleToChange}
+            className="border-current border-2 h-12 w-full rounded-md outline-none"
+          >
+            <option value="">None</option>
             {Object.keys(currency).map((key, index) => (
-              <option key={index} value={key}>
-                {key}
+              <option key={index} value={key} onChange={handleFromChange}>
+                {key}- {currency[key]}
               </option>
             ))}
           </select>
